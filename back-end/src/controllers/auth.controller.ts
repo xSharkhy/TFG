d00fd1f1e0ef20d2418e0311d9438707e4a85b2b
xSharkhy <sharkhyacc@gmail.com>
2023-05-05 00:54:16 +0200

    chore(user): improve user model and auth security

--- back-end/src/controllers/auth.controller.ts
+++ back-end/src/controllers/auth.controller.ts
@@ -1,38 +1,41 @@
-// auth.controller.ts
 import { Request, Response } from 'express';
 import User from '../models/user.model';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 class AuthController {
-	async signup(req: Request, res: Response) {
-		try {
-			const user = User.build(req.body);
-			await user.save();
-			res.status(201).send(user);
-		} catch (error) {
-			res.status(400).send(error);
-		}
-	}
+    async signup(req: Request, res: Response) {
+        try {
+            const user = User.build(req.body);
+            await user.save();
+            const userWithoutPassword = user.toObject();
+            delete userWithoutPassword.password;
+            res.status(201).send(userWithoutPassword);
+        } catch (error) {
+            res.status(400).send(error);
+        }
+    }
 
-	async login(req: Request, res: Response) {
-		try {
-			const user = await User.findOne({ email: req.body.email });
-			if (!user) {
-				return res.status(400).send('Invalid email or password');
-			}
 
-			const isMatch = await bcrypt.compare(req.body.password, user.password);
-			if (!isMatch) {
-				return res.status(400).send('Invalid email or password');
-			}
+    async login(req: Request, res: Response) {
+        try {
+            const user = await User.findOne({ email: req.body.email });
+            if (!user) {
+                return res.status(400).send('Invalid email or password');
+            }
 
-			const token = jwt.sign({ _id: user._id }, 'your_jwt_secret');
-			res.send({ token });
-		} catch (error) {
-			res.status(400).send(error);
-		}
-	}
+            const isMatch = await bcrypt.compare(req.body.password, user.password);
+            if (!isMatch) {
+                return res.status(400).send('Invalid email or password');
+            }
+
+            const token = jwt.sign({ _id: user._id }, 'your_jwt_secret');
+            res.send({ token });
+        } catch (error) {
+            res.status(400).send('Invalid email or password');
+        }
+    }
+
 }
 
 export default new AuthController();
--- back-end/src/middleware/jwt.middleware.ts
+++ back-end/src/middleware/jwt.middleware.ts
@@ -2,24 +2,27 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 interface DecodedUser {
-	_id: string;
+    _id: string;
 }
 
-export const jwtMiddleware = (
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => {
-	const token = req.header('Authorization')?.replace('Bearer ', '');
-	if (!token) {
-		return res.status(401).send('Access denied. No token provided.');
-	}
+const jwtSecret = process.env.JWT_SECRET || 'your_jwt_secret';
 
-	try {
-		const decoded = jwt.verify(token, 'your_jwt_secret') as DecodedUser;
-		req.user = { _id: decoded._id };
-		next();
-	} catch (error) {
-		res.status(400).send('Invalid token.');
-	}
+export const jwtMiddleware = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+        return res.status(401).send('Access denied. No token provided.');
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret) as DecodedUser;
+        req.user = { _id: decoded._id };
+        next();
+    } catch (error) {
+        res.status(400).send('Invalid token.');
+    }
 };
+
