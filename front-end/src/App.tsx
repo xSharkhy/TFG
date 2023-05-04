01a08e204ac22201d14394e55ab0c22f0b410998
xSharkhy <sharkhyacc@gmail.com>
2023-05-04 16:05:54 +0200

    fix(routes) - basic routes handle
    installed custom colors on tailwindcss to start developing the frontend

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PhaserGame from "./PhaserGame";
+import LoginForm from "./components/LoginForm";
+import SignupForm from "./components/SignupForm";
 
 const App: React.FC = () => {
-	return (
-		<>
-			<PhaserGame />
-		</>
-	);
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" Component={LoginForm} />
+                <Route path="/signup" Component={SignupForm} />
+                <Route path="/game" Component={PhaserGame} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
